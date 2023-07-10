79eed1e7dba46a4b17787f0e6d382b9805fb0f79
Sergey Myshlenok <smdosautotrans@mail.ru>
2023-07-10 21:36:05 +0300

    задачи_8 конец

--- Лекция_4/работа с файлами.py
+++ Лекция_4/работа с файлами.py
@@ -108,3 +108,21 @@
 #     ● shutil.copyfile(src, dst) - копирует содержимое (но не метаданные) файла src в файл dst.
 #     ● shutil.copy(src, dst) - копирует содержимое файла src в файл или папку dst.
 #     ● shutil.rmtree(path) - Удаляет текущую директорию и все поддиректории; path должен указывать на директорию, а не на символическую ссылку.
+
+
+#w - запись
+#r - чтение
+#a - добавить
+# a = ["q\n","r\n","e\n"]
+# with open("input.txt","w") as file:
+#     file.write("strq\n")
+#     file.writelines(a)
+#with open("input.txt","r") as file:
+    # print(file.read())
+    # file.seek(0)
+    # print(file.readlines())
+    # for i in range(4):
+    #     print(file.readline(i))#
+    #a = list(map(lambda x: x.strip(),file.readlines()))
+# with open("input.txt","a") as file:
+#     file.write("qw")
